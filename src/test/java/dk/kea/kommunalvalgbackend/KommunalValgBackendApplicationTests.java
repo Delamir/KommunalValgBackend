ce1ab050edd358ffc30458a40ba745238b540fb3
Delamir <td_christian@hotmail.com>
2021-12-20 09:28:09 +0100

    Initial commit

---
+++ src/test/java/dk/kea/kommunalvalgbackend/KommunalValgBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.kommunalvalgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KommunalValgBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
